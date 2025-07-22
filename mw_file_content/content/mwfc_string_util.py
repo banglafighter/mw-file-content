08e7ac0874cca474676e219573007b73aa617f7b
H.M.Touhid Mia (HMTMCSE) <hmtm.cse@gmail.com>
2025-07-22 13:09:08 +0600

    Add Text to URL

--- mw_file_content/content/mwfc_string_util.py
+++ mw_file_content/content/mwfc_string_util.py
@@ -4,26 +4,26 @@ from copy import copy
 
 class StringUtil:
 
-    @staticmethod
-    def find_and_replace_with(text: str, find: any, replace: any):
+    @classmethod
+    def find_and_replace_with(cls, text: str, find: any, replace: any):
         text = copy(text)
         return text.replace(find, replace)
 
-    @staticmethod
-    def lower_first_char(text: str) -> str:
+    @classmethod
+    def lower_first_char(cls, text: str) -> str:
         return text[0].lower() + text[1:] if text else ""
 
-    @staticmethod
-    def camelcase_to(text: str, to: str = "_") -> str:
+    @classmethod
+    def camelcase_to(cls, text: str, to: str = "_") -> str:
         text = text.strip()
         return re.sub(r'(?<!^)(?=[A-Z])', to, text)
 
-    @staticmethod
-    def replace_multiple_occurrence_to_single_with(text: str, to: str = "_") -> str:
+    @classmethod
+    def replace_multiple_occurrence_to_single_with(cls, text: str, to: str = "_") -> str:
         return re.sub(f"{re.escape(to)}+", to, text)
 
-    @staticmethod
-    def system_readable(text: str) -> str:
+    @classmethod
+    def system_readable(cls, text: str) -> str:
         text = StringUtil.camelcase_to(text, "_")
         text = StringUtil.find_and_replace_with(text, " ", "_")
         text = StringUtil.find_and_replace_with(text, "-", "_")
@@ -31,28 +31,28 @@ class StringUtil:
         text = re.sub(r'[^a-zA-Z0-9_]', '', text)
         return text.strip().lower()
 
-    @staticmethod
-    def remove_special_character(text: str, to: str = "") -> str:
-        return re.sub(r'[^\w\s/\-]', to, text)
+    @classmethod
+    def remove_special_character(cls, text: str, to: str = "") -> str:
+        return re.sub(r'[^\w\s/\-\u0980-\u09FF]', to, text)
 
-    @staticmethod
-    def remove_leading_number(text: str) -> str:
+    @classmethod
+    def remove_leading_number(cls, text: str) -> str:
         return re.sub(r"^\d+", '', text)
 
-    @staticmethod
-    def py_underscore_name(name: str) -> str:
+    @classmethod
+    def py_underscore_name(cls, name: str) -> str:
         name = StringUtil.lower_first_char(name)
         name = StringUtil.system_readable(name)
         name = StringUtil.remove_special_character(name)
         name = StringUtil.remove_leading_number(name)
         return name
 
-    @staticmethod
-    def replace_space_with(text: str, to: str = "_"):
+    @classmethod
+    def replace_space_with(cls, text: str, to: str = "_"):
         return re.sub(r'\s+', to, text)
 
-    @staticmethod
-    def human_readable(text: str, default=None):
+    @classmethod
+    def human_readable(cls, text: str, default=None):
         if text is None:
             return default
         text = StringUtil.camelcase_to(copy(text), " ")
@@ -60,3 +60,17 @@ class StringUtil:
         text = text.strip()
         text = text.title()
         return StringUtil.replace_space_with(text, " ")
+
+    @classmethod
+    def text_to_url_text(cls, text: str, default=None):
+        if not text:
+            return default
+        text = cls.camelcase_to(copy(text), "-")
+        text = cls.find_and_replace_with(text, " ", "-")
+        text = cls.find_and_replace_with(text, "_", "-")
+        text = cls.replace_multiple_occurrence_to_single_with(text=text, to="-")
+        text = cls.remove_special_character(text)
+        text = text.strip()
+        text = text.strip("-")
+        text = text.lower()
+        return text
